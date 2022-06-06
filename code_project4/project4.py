788b1b9ae3901e1459f08e4acf3477a48c22f7d2
andregomidess <andreluccag@gmail.com>
2022-06-06 15:40:51 -0300

    Update project4.py
    arrumando o boneco

--- code_project4/project4.py
+++ code_project4/project4.py
@@ -10,55 +10,79 @@ clear = lambda: os.system('clear')
 def imprime_boneco(tentativas):
     if tentativas == 7:
         print("""\033[1;96m
-                    O  
-                   /|\\
+              _______
                     |
-                   / \\
+                     
+                   
+                
+                   
                             \033[m""")
     elif tentativas == 6:
         print("""\033[1;96m
+              _______
+                    |
+                    O  
+                   
+                
+                   
+                            \033[m""")
+        
+    elif tentativas == 5:
+        print("""\033[1;96m
+              _______
+                    |
+                    O  
+                    |
+                    
+                   
+                            \033[m""")
+    elif tentativas == 4:
+        print("""\033[1;96m
+              _______
+                    |
+                    O  
+                    |
+                    |
+                   
+                            \033[m""")
+    elif tentativas == 3:
+        print("""\033[1;96m
+              _______
+                    |
+                    O  
+                   /|
+                    |
+                   
+                            \033[m""")
+    elif tentativas == 2:
+        print("""\033[1;96m
+              _______
+                    |
+                    O  
+                   /|\\
+                    |
+                   
+                            \033[m""")
+        
+    elif tentativas == 1:
+        print("""\033[1;96m
+              _______
+                    |
                     O  
                    /|\\
                     |
                    / 
                             \033[m""")
         
-    elif tentativas == 5:
-        print("""\033[1;96m
-                    O  
-                   /|\\
-                    |
-
-                            \033[m""")
-    elif tentativas == 4:
-        print("""\033[1;96m
-                    O  
-                   /|\\
-                    
-
-                            \033[m""")
-    elif tentativas == 3:
-        print("""\033[1;96m
-                    O  
-                   /|
-                    
-
-                            \033[m""") 
-    elif tentativas == 2:
-        print("""\033[1;96m
-                    O  
-                    |
-                    
-
-                            \033[m""")
-        
     else:
         print("""\033[1;96m
+              _______
+                    |
                     O  
-                   
-                    
-
-                            \033[m""")
+                   /|\\
+                    |
+                   / \\
+                            \033[m""")    
                                
             
     
@@ -70,23 +94,17 @@ def palavras_aleatorias(words):
     return palavra.upper()
 
 def menu():
-    tentativas = 7
+    tentativas = 0
     print('=-'*18)
     print('          |JOGO DA FORCA|')
     imprime_boneco(tentativas)
     print('Digite [1] para jogar')
     print('Digite [2] para sair')
-    op = input('Digite sua opção: ')
-    if op == 1:
-        pass
-    else:
-        pass
     print('=-'*18)
     
     
 def jogo_forca(palavra):
-    #print(palavra)
-    acertos = 0
+    print(palavra)
     tam_word = len(palavra)
     tentativas = 7
     simbolos = list()
@@ -95,28 +113,46 @@ def jogo_forca(palavra):
     print(f'A PALAVRA TEM {tam_word} LETRAS!')
     print(f'NÚMERO DE TENTATIVAS: {tentativas}')
     sleep(1)
-    #print('     \033[1;91m__  \033[m'*tam_word)
     for _ in range(tam_word):
         simbolos.append('\033[1;91m__\033[m')
     t = ' '.join(simbolos)      
-    while tentativas > 0 or acertos >= tam_word:
+    while tentativas > 0:
+        if simbolos == letras_palavra:
+            break 
+        lixo = ' '.join(letras_digitadas)
+        print(f'Letras digitadas: {lixo}')
         print(t)
         imprime_boneco(tentativas)
-        letra = input('Digite uma letra: ').upper()               
-        if letra in letras_digitadas:
+        letra = input('Digite uma letra: ').upper()
+        while len(letra) > 1:
+            print('\033[1;31mDIGITE APENAS UMA LETRA!\033[m')
+            letra = input('Digite uma letra: ').upper()               
+        while letra in letras_digitadas:
             print('\033[1;31mLETRA JÁ DIGITADA!\033[m')
-            
+            letra = input('Digite outra letra: ').upper()
         letras_digitadas.append(letra)
         if letra in palavra:    # fazer função checa letra
             for i in range(tam_word):
                 if letra == letras_palavra[i]:
                     simbolos[i] = letras_palavra[i]
                     t = ' '.join(simbolos)
-                    print(t)
-                    acertos += 1
+                    print(t)       
         else:
             tentativas -= 1
-        clear()                   
+        clear()
+        
+    if simbolos == letras_palavra:
+        print(t)
+        imprime_boneco(tentativas)
+        print('\033[1;92mPARABÉNS VOCÊ GANHOU!\033[m\n')
+    
+    else:
+        print(t)
+        imprime_boneco(tentativas)
+        print('\033[1;31mQUE PENA, VOCÊ PERDEU!\033[m\n')
+        print(f'A palavra era: \033[1;96m{palavra}\033[m')   
+        
+                           
                     
             
             
@@ -124,6 +160,12 @@ def jogo_forca(palavra):
 if __name__ == '__main__':
     palavra = palavras_aleatorias(words)
     menu()
-    clear()
-    jogo_forca(palavra)
+    op = int(input('Digite sua opção: '))
+    if op == 1:
+        clear()
+        jogo_forca(palavra)
+    else:
+        pass
+    
+    print()
             
